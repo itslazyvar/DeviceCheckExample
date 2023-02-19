8e63e84ca83531401821d627dd1a4088ae40afc7
thattridentdude <5077094+thattridentdude@users.noreply.github.com>
2023-02-19 00:26:07 -0800

    Initial Commit

---
+++ DeviceCheck Example/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DeviceCheck Example
+//
+//  Created by D.B. Galeli on 2/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DeviceCheck Example/DeviceCheck_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceCheck_ExampleApp.swift
+//  DeviceCheck Example
+//
+//  Created by D.B. Galeli on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct DeviceCheck_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
